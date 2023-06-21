dda4021b73e69fe17c2ea75550639e63058adb61
Max Pla1n <faterin01032004@gmail.com>
2023-06-21 22:29:11 +0300

    roman

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ const two = document.querySelector('.second_summand')
 const action = document.querySelectorAll('a')
 const output = document.querySelector('.output')
 
+let arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]; 
+let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+function fromArabicToRoman(a){
+  let result = '';
+  let num = Number(a)
+  arabic.map( (item, index) => {
+    while (item <= num){
+      result += roman[index]
+      num -= item
+    }
+  });
+  return result
+}
+
+function fromRomanToArabic(r){
+  let num = r.split("").reverse().join("")
+  let i = 0
+  let result = 0
+  while (i <= num.length - 1){
+    let j = i + 1
+    while (arabic[roman.indexOf(num[i])] > arabic[roman.indexOf(num[j])] && j <= num.length){ 
+      result -= arabic[roman.indexOf(num[j])]
+      j += 1
+    }
+  result += arabic[roman.indexOf(num[i])]
+  i = j
+  }
+  return result
+}
+
 const sum = (a, b) => {
   return a + b
 }
@@ -50,35 +81,59 @@ acButton.addEventListener('click', clear)
 const actionBtn = event => {
   const target = event.target;
 
-  const numberOne = Number(one.value)
-  const numberTwo = Number(two.value)
+  let numberOne = one.value
+  let numberTwo = two.value
 
+  let isArabic
+  let result 
+
+  if (!Number(numberOne) && !Number(numberTwo)){
+    numberOne = fromRomanToArabic(numberOne.toUpperCase())
+    numberTwo = fromRomanToArabic(numberTwo.toUpperCase())
+    isArabic = false
+  }
+  else{
+    numberOne = Number(numberOne)
+    numberTwo = Number(numberTwo)
+    isArabic = true
+  }
+  console.log(numberOne)
+  console.log(numberTwo)
   if (!one.value || !two.value){
     output.textContent = 'Вы внесли не все данные'
   }
+  else if (!numberOne || !numberTwo){
+    output.textContent = 'Вы внесли неправильные значения'
+  }
   else{
     if (target.classList.contains("plus")){
-      output.textContent = sum(numberOne, numberTwo)
+      result = sum(numberOne, numberTwo)
+      output.textContent = isArabic ? result : fromArabicToRoman(result)
     }
 
     if (target.classList.contains("minus")){
-      output.textContent  = subtraction(numberOne, numberTwo)
+      result = subtraction(numberOne, numberTwo)
+      output.textContent = isArabic ? result : fromArabicToRoman(result)
     }
 
     if (target.classList.contains("percent")){
-      output.textContent = percent(numberOne, numberTwo)
+      result = percent(numberOne, numberTwo)
+      output.textContent = isArabic ? result : fromArabicToRoman(result)
     }
 
     if (target.classList.contains("multi")){
-      output.textContent = multiplication(numberOne, numberTwo)
+      result = multiplication(numberOne, numberTwo)
+      output.textContent = isArabic ? result : fromArabicToRoman(result)
     }
 
     if (target.classList.contains("division")){
-      output.textContent = division(numberOne, numberTwo)
+      result = division(numberOne, numberTwo)
+      output.textContent = isArabic ? result : fromArabicToRoman(result)
     }
 
     if (target.classList.contains("power")){
-      output.textContent = pow(numberOne, numberTwo)
+      result = pow(numberOne, numberTwo)
+      output.textContent = isArabic ? result : fromArabicToRoman(result)
     }  
   }
 }
@@ -86,4 +141,3 @@ const actionBtn = event => {
 action.forEach( element => {
   element.addEventListener('click', actionBtn)
 })
-
